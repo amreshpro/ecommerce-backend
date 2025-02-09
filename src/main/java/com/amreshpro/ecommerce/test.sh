d112a7be3b86db5e0ce6b4ee099b403bb1c7673c
Amresh Maurya <amresh.terminal@gmail.com>
2025-02-09 18:01:52 +0530

    rewrite

---
+++ src/main/java/com/amreshpro/ecommerce/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#cd ./mapper
+#touch ProductMapper.java OrderMapper.java CategoryMapper.java UserMapper.java
+#
+#cd ../service
+#touch ProductService.java OrderService.java CategoryService.java UserService.java
+#
+#cd ../repository
+#touch ProductRepository.java OrderRepository.java CategoryRepository.java UserRepository.java
+#
+#cd ../controller
+#touch ProductController.java OrderController.java CategoryController.java UserController.java
+#
+
+cd ./service/impl/
+
+touch ProductServiceImpl.java OrderServiceImpl.java CategoryServiceImpl.java UserServiceImpl.java
